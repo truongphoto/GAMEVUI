5d7d13ce69c0fa75c25dd9df99051125a6cf512f
truongphoto <154951060+truongphoto@users.noreply.github.com>
2026-08-24 10:03:10 +0700

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "gpp-relax-pa1-pa2-final-v2-3-4";
+const CACHE_NAME = "gpp-relax-pa1-pa2-final-v2-3-5";
 const BASE = new URL("./", self.registration.scope);
 const CORE_FILES = [
   "./",
